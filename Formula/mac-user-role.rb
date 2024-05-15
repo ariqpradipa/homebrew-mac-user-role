e70ae20ad76e6fca93e0816168a3eb0ac29534a4
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2024-05-15 10:42:01 +0700

    update url

--- Formula/mac-user-role.rb
+++ Formula/mac-user-role.rb
@@ -1,7 +1,7 @@
 class MacUserRole < Formula
     desc "Script to change user roles between admin and user on macOS"
-    homepage "https://your.homepage.url"
-    url "./mac-user-role.sh" # Change this to the path where your script is stored
+    homepage "https://arprd.com"
+    url "https://raw.githubusercontent.com/ariqpradipa/homebrew-mac-user-role/main/main/scripts/mac-user-role.sh" # Change this to the path where your script is stored
     version "1.0.0"
     sha256 "68bc174d9fc15fd255f4799f3e784f8d123c1d01f4f287ce71c71434c427bcc6" # Use the actual checksum you got
   
